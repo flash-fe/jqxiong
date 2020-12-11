3bf24c3e27f2b17458f17e60abc397bcc63c2fbf
jqxiong <jqxiong@trip.com>
2020-12-11 18:02:53 +0800

    update pack

--- packages/web-basic/build/copy.js
+++ packages/web-basic/build/copy.js
@@ -3,11 +3,14 @@ const fs = require('fs')
 const path = require('path')
 
 const JSONStr = fs.readFileSync(path.resolve(__dirname, '..', 'package.json'), { encoding: 'utf8' });
+const JSONDist = fs.readFileSync(path.resolve(__dirname, '..', 'dist', 'package.json'), { encoding: 'utf8' });
 
 const preJSON = JSON.parse(JSONStr)
+const perDistJSON = JSON.parse(JSONDist); // 保持版本号
 
 // 加上前缀
 preJSON.name = `@jqxiong/${preJSON.name}`;
+preJSON.version = perDistJSON.version;
 
 delete preJSON.devDependencies
 
